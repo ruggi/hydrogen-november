fba1dbd136911c41f96c26a932848432291c72c7
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-04-23 16:12:41 +0200

    TrippyButton annotation

--- utopia/components.utopia.js
+++ utopia/components.utopia.js
@@ -18,6 +18,7 @@ import {
   Placeholder,
   SectionTitle,
   SectionSubtitle,
+  TrippyButton,
 } from '../app/components/Components'
 
 const RowVariants = [
@@ -169,7 +170,6 @@ const ColumnVariants = [
   },
 ]
 
-// HalfAndHalf,
 // TrippyButton,
 // SpecialRow,
 // TwoFeatureCallout,
@@ -263,6 +263,57 @@ const Components = {
         padded: Utopia.checkboxControl(),
         gap: Utopia.numberControl(),
       },
+    },
+    TrippyButton: {
+      component: TrippyButton,
+      properties: {
+        price: {
+          control: 'jsx',
+          preferredContents: [
+            {
+              component: 'Price',
+              moduleName: '@shopify/hydrogen',
+              variants: [
+                {
+                  label: 'Price with Money component',
+                  imports:
+                    'import { Money } from "@shopify/hydrogen"',
+                  code: `<Money data={{
+                    amount: '59',
+                    currencyCode: 'HUF',
+                  }} />`,
+                },
+              ],
+            },
+          ],
+        },
+      },
+      focus: 'never',
+      inspector: ['typography'],
+      variants: [
+        {
+          label: 'TrippyButton',
+          imports: `import { TrippyButton } from "/app/components/Components"`,
+          code: '<TrippyButton />',
+        },
+        {
+          label: 'TrippyButton with number price',
+          imports: `import { TrippyButton } from "/app/components/Components"`,
+          code: `<TrippyButton price={'59.00'} />`,
+        },
+        {
+          label: 'TrippyButton with Money price',
+          imports: `import { TrippyButton } from "/app/components/Components"
+            import { Money } from "@shopify/hydrogen"`,
+          code: `<TrippyButton
+            price={
+              <Money
+                data={{ amount: '59', currencyCode: 'HUF' }}
+              />
+            }
+          />`,
+        },
+      ],
     },
     Placeholder: {
       component: Placeholder,

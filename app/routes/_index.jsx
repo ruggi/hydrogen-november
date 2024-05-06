f316835632953a89f36ca0ceab5692506404aa7d
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2024-05-06 17:19:09 +0200

    removing leftover "template image"

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -80,19 +80,6 @@ export default function LandingPage() {
 
   return (
     <Column>
-      <div
-        style={{
-          display: 'none',
-          opacity: 0.5,
-          position: 'absolute',
-          zIndex: -1,
-          top: -64,
-          width: 1440,
-          height: 9599,
-          backgroundImage:
-            'url(https://cdn.discordapp.com/attachments/703248722334842910/1231926200054583316/trippytrails.jpg?ex=6638bbae&is=662646ae&hm=0e5a7fd4ed51a811416af3dd6a93f8d0baf5b3956a3023bb4714579c81d597c0&)',
-        }}
-      />
       <Spacer height='4em' />
       <Section padded>
         <HalfAndHalf

fc2c73aa66e8c4017c8ff18cac463046a978af74
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-06-17 15:05:07 +0200

    update

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -87,7 +87,7 @@ export default function LandingPage() {
             left={
               <Column style={{ gap: 24 }}>
                 <TypographyTitle level={1}>
-                  We produce pretty NICE bags for lifestyle
+                  We produce pretty COOL! bags for lifestyle
                 </TypographyTitle>
                 <TrippyButton
                   price={
